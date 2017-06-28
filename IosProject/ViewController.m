f7142ffea0c03f8ff56c6264d954106c213961ca
meZS <798506095@qq.com>
2017-06-28 17:23:35 +0800

    ..

--- IosProject/ViewController.m
+++ IosProject/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "UserModel.h"
 @interface ViewController ()
 
 @end
@@ -17,7 +17,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    NSDictionary *dict = @{@"userName":@"Hello",@"userImg":@"HI",@"userID":@"33",@"student":@{@"stuA":@"阿狸",@"stuB":@"小法"},@"person":@[@{@"perA":@"轮子妈",@"perB":@"奥巴马"},@{@"perA":@"老鼠",@"perB":@"瞎子"},@{@"perA":@"金克斯",@"perB":@"女警"}]};
+    
+    UserModel *uModel = [[UserModel alloc] initWithDictionary:dict];
+    
+    NSLog(@"\n====%@\n====%@\n==%@",uModel.userName,uModel.userID,uModel.userImg);
+    
+    NSLog(@"\n===%@\n==%@", uModel.student.stuA,uModel.student.stuB);
+    
+    
+    
+    NSLog(@"\n==%@\n==%@\n==%@\n==%@\n==%@\n==%@",uModel.person[0].perA,uModel.person[0].perB,uModel.person[1].perA,uModel.person[1].perB,uModel.person[2].perA,uModel.person[2].perB);
+    
+    
+    
 }
+
+
 
 
 - (void)didReceiveMemoryWarning {
