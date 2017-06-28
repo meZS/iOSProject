d20771e1219ea6bb4ae751f554dc391d80a00040
meZS <798506095@qq.com>
2017-06-28 14:06:53 +0800

    Initial Commit

---
+++ IosProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IosProject
+//
+//  Created by wordy on 2017/6/28.
+//  Copyright © 2017年 golddatacommunications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
