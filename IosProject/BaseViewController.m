3af9dd5c89c8b5d8a82e314458aeffb7ada40f84
meZS <798506095@qq.com>
2017-06-29 09:58:01 +0800

    跟新部分代码

--- IosProject/BaseViewController.m
+++ IosProject/BaseViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor cyanColor];
+    self.title = @"我是继承来的😄";
 }
 
 - (void)didReceiveMemoryWarning {
